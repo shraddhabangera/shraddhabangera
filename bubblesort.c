085d9efdc2014fa2634ed01e0e02fb7e1e54a6e4
shraddha1410 <100298384+shraddhabangera@users.noreply.github.com>
2022-03-04 16:22:53 +0530

    Update bubblesort.c

--- bubblesort.c
+++ bubblesort.c
@@ -1,14 +1,8 @@
 #include<stdio.h>
-void main()
+int a[10],i,n;
+void bubble()
 {
-int a[10],i,j,n,temp=0;
-printf("Enter the limit\n");
-scanf("%d",&n);
-printf("Enter the elements\n");
-for(i=0;i<n;i++)
-{
-scanf("%d",&a[i]);
-}
+int j,temp=0;
 for(i=0;i<n-1;i++)
 {
 for(j=0;j<n-i-1;j++)
@@ -21,6 +15,17 @@ a[j+1]=temp;
 }
 }
 }
+}
+void main()
+{
+printf("Enter the limit\n");
+scanf("%d",&n);
+printf("Enter the elements\n");
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+bubble();
 printf("Sorted elements are\n");
 for(i=0;i<n;i++)
 {
