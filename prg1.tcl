668fd6742ac31394289ac04c362b723ce1d00de1
shraddha1410 <100298384+shraddhabangera@users.noreply.github.com>
2022-04-05 16:26:22 +0530

    CN lab

---
+++ prg1.tcl
@@ -0,0 +1,82 @@
+# time of simulation end
+
+#===================================
+#        Initialization        
+#===================================
+#Create a ns simulator
+set ns [new Simulator]
+
+#Open the NS trace file
+set tracefile [open out.tr w]
+$ns trace-all $tracefile
+
+#Open the NAM trace file
+set namfile [open out.nam w]
+$ns namtrace-all $namfile
+
+#===================================
+#        Nodes Definition        
+#===================================
+#Create 3 nodes
+set n0 [$ns node]
+set n1 [$ns node]
+set n2 [$ns node]
+
+#===================================
+#        Links Definition        
+#===================================
+#Createlinks between nodes
+$ns duplex-link $n0 $n1 1000Kb 10ms DropTail
+$ns queue-limit $n0 $n1 50
+$ns duplex-link $n1 $n2 500Kb 10ms DropTail
+$ns queue-limit $n1 $n2 50
+
+#Give node position (for NAM)
+$ns duplex-link-op $n0 $n1 orient right
+$ns duplex-link-op $n1 $n2 orient right
+
+#===================================
+#        Agents Definition        
+#===================================
+set udp0 [new Agent/UDP]
+$ns attach-agent $n0 $udp0
+set null1 [new Agent/Null]
+$ns attach-agent $n2 $null1
+$ns connect $udp0 $null1
+$udp0 set packetSize_ 1000
+
+#===================================
+#        Applications Definition        
+#===================================
+#Setup a CBR Application over UDP connection
+set cbr0 [new Application/Traffic/CBR]
+$cbr0 attach-agent $udp0
+$cbr0 set packetSize_ 1000
+$cbr0 set rate_ 1.0Mb
+$cbr0 set random_ null
+$ns at 1.0 "$cbr0 start"
+$ns at 2.0 "$cbr0 stop"
+
+
+#===================================
+#        Termination        
+#===================================
+#Define a 'finish' procedure
+proc finish {} {
+    global ns tracefile namfile
+    $ns flush-trace
+    close $tracefile
+    close $namfile
+    exec nam out.nam &
+    exit 0
+}
+
+
+
+
+
+
+
+$ns at 10.0 "finish"
+$ns run
+
